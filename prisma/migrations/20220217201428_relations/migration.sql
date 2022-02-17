999f095b3aa48fc3f0f8425d127be3f78be20d15
luchx88 <lucianocavallo@gmail.com>
2022-02-17 17:58:27 -0300

    prisma migration for relations between posts users and comments. Mutations for createUser and createPost

---
+++ prisma/migrations/20220217201428_relations/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Comment` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Comment" ADD COLUMN     "userId" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "usersLikesId" INTEGER[];
+
+-- CreateTable
+CREATE TABLE "_LikedPosts" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_LikedPosts_AB_unique" ON "_LikedPosts"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_LikedPosts_B_index" ON "_LikedPosts"("B");
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LikedPosts" ADD FOREIGN KEY ("A") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LikedPosts" ADD FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,4 @@
-import type { PrismaClient } from '@prisma/client';
+import type { PrismaClient, User } from '@prisma/client';
 
 type ResolverContext = {
   orm: PrismaClient;
@@ -6,13 +6,40 @@ type ResolverContext = {
 
 export const resolvers = {
   Query: {
-    users: async (parent: unknown, args: unknown, context: ResolverContext) => {
-      const { orm } = context;
-      return await orm.user.findMany();
+    users: async (parent: unknown, args: unknown, { orm }: ResolverContext) => {
+      return await orm.user.findMany({
+        include: {
+          posts: true,
+        },
+      });
     },
-    posts: async (parent: unknown, args: unknown, context: ResolverContext) => {
-      const { orm } = context;
-      return await orm.post.findMany();
+    posts: async (parent: unknown, args: unknown, { orm }: ResolverContext) => {
+      return await orm.post.findMany({
+        include: {
+          user: true,
+        },
+      });
+    },
+  },
+  Mutation: {
+    createUser: async (
+      parent: unknown,
+      { input }: { input: User },
+      { orm }: ResolverContext
+    ) => {
+      return await orm.user.create({ data: input });
+    },
+    createPost: async (
+      parent: unknown,
+      { input }: { input: { body: string; userId: string } },
+      { orm }: ResolverContext
+    ) => {
+      return await orm.post.create({
+        data: {
+          body: input.body,
+          userId: parseInt(input.userId),
+        },
+      });
     },
   },
 };
