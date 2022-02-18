1748fd06ad90aa4e911d134566d08878b0a5c67b
luchx88 <lucianocavallo@gmail.com>
2022-02-17 22:49:18 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import { login } from './auth';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use('/api/login', login);
 
